99bfb593f704f4e6d93fbac43e3d7b74baa4c34f
I524764 <raviganesh.maniyoor@sap.com>
2021-07-05 19:03:14 +0530

    Initial app structure

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["com/myorg/documentSaver/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.myorg.documentSaver.controller.MainView", {});
+});
